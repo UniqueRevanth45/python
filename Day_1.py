c4c80ec9661863e5b2dcbcc10851f041bb06653a
UniqueRevanth45 <yerrarevanth9@gmail.com>
2025-01-23 19:18:15 +0530

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Integer
+a=int(input("Enter an integer: "))
+print(type(a))
+
+
+# In[3]:
+
+
+#Float
+b=float(input("Enter a Floating point number: "))
+print(type(b))
+
+
+# In[4]:
+
+
+#String
+c=input("Enter a String: ")
+print(type(c))
+
+
+# In[6]:
+
+
+#Boolean
+d=True
+print(type(d))
+
+
+# In[7]:
+
+
+a=int(input())
+b=float(input())
+c=input()
+d=False
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
