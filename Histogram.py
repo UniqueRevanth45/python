4d2f07f195634b56fbfa103eb5bfa62376869705
UniqueRevanth45 <yerrarevanth9@gmail.com>
2025-02-05 20:01:13 +0530

    Add files via upload

---
+++ Histogram.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(42)  
+data = np.random.randint(20, 81, 1000)
+plt.hist(data, bins=15, edgecolor='black', color='skyblue')
+plt.title('Histogram of Cancer Patients Age Distribution')
+plt.xlabel('Age')
+plt.ylabel('Number of Patients')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
